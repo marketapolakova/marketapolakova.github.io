0ca9ca19f932298972618ad057069ad8fbb1b7d3
Marketa Polakova <polakovamarketa@email.cz>
2022-02-05 17:57:00 +0100

    scissors rock paper game add

---
+++ rock-paper-scissors-game/design/index.js
@@ -0,0 +1,70 @@
+//open rules
+document.querySelector('.btn-rules').addEventListener('click', function () {
+    document.querySelector('.rules').style.display = 'block';
+});
+
+//close rules
+document.querySelector('.btn-close').addEventListener('click', function () {
+    document.querySelector('.rules').style.display = 'none';
+});
+
+game()
+//random generator scissors paper rock
+function game() {
+    //selecting rock paper scissors
+    let select = ['paper', 'scissors', 'rock'];
+    let playerChoice;
+    let computerChoice = '';
+    let score=0;
+    for (let index = 0; index < select.length; index++) {
+
+        document.querySelector('.' + select[index]).addEventListener('click', function () {
+            let random = Math.floor(Math.random() * 3)
+            playerChoice = select[index];
+            computerChoice = select[random];
+            //displaying player choice
+            document.querySelector('.background').style.display = 'none';
+            document.querySelector('.picked').style.display = 'block';
+            document.querySelector('.picked-' + select[index]).style.display = 'block';
+            //displaying computer choice
+            setTimeout(function () {
+                document.querySelector('.computer-none').style.display = 'none';
+                document.querySelector('.computer-' + select[random]).style.display = 'block';
+                document.querySelector('.result').style.display = 'block';
+            //compare compuperChoice and playerChoice and display result
+            if (computerChoice === playerChoice) {
+                document.querySelector(".heading-result").innerHTML = "Draw";
+               
+            } else if (computerChoice === 'paper' && playerChoice === 'scissors' || computerChoice === 'rock' && playerChoice === 'paper' || computerChoice === 'scissors' && playerChoice === 'rock') {
+                document.querySelector(".heading-result").innerHTML = "You win";
+                document.querySelector(".circle-container").style.display = "block";
+                score++;
+            } else if (computerChoice === 'scissors' && playerChoice === 'paper' || computerChoice === 'rock' && playerChoice === 'scissors' || computerChoice === 'paper' && playerChoice === 'rock') {
+                document.querySelector(".heading-result").innerHTML = "You lose";
+                document.querySelector(".computer-circle-container").style.display = "block";
+                score--;
+            }
+            //display score
+                document.querySelector(".number").innerHTML = score;
+            }, 1000);
+           
+        });
+    }
+}
+
+//new game
+document.querySelector('.play').addEventListener('click', function () {
+    document.querySelector('.background').style.display = 'block';
+    document.querySelector('.picked').style.display = 'none';
+    document.querySelector('.picked-paper').style.display = 'none';
+    document.querySelector('.picked-scissors').style.display = 'none';
+    document.querySelector('.picked-rock').style.display = 'none';
+    document.querySelector('.computer-paper').style.display = 'none';
+    document.querySelector('.computer-scissors').style.display = 'none';
+    document.querySelector('.computer-rock').style.display = 'none';
+    document.querySelector('.computer-none').style.display = 'block';
+    document.querySelector('.result').style.display = 'none';
+    document.querySelector(".computer-circle-container").style.display = "none";
+    document.querySelector(".circle-container").style.display = "none";
+
+});
